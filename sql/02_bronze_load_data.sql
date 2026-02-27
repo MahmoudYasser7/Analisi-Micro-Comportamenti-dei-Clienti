92cb3cff23f96a80d011ee1b175c5dfa84628e7e
Mahmoud Yasser <Mahmoud.yasser771@yahoo.com>
2026-02-27 16:02:00 +0100

    Create 02_bronze_load_data.sql

---
+++ sql/02_bronze_load_data.sql
@@ -0,0 +1,43 @@
+USE Analisi;
+GO
+
+BULK INSERT dbo.[bronze customersup]
+FROM './data/Customersup.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\r\n',
+    TABLOCK,
+    ERRORFILE = './data/Customersup_error.log',
+    DATAFILETYPE = 'char',
+    CODEPAGE = '65001'
+);
+GO
+
+    
+BULK INSERT dbo.[bronze bookings]
+FROM './data/Bookings.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\r\n',
+    TABLOCK,
+    ERRORFILE = './data/Bookings_error.log',
+    DATAFILETYPE = 'char',
+    CODEPAGE = '65001'
+);
+GO
+
+    
+BULK INSERT dbo.[bronze ancillaries]
+FROM './data/Ancillaries.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\r\n',
+    TABLOCK,
+    ERRORFILE = './data/Ancillaries_error.log',
+    DATAFILETYPE = 'char',
+    CODEPAGE = '65001'
+);
+GO
